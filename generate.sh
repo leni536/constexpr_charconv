ea91f57c864cc6f333930aee75fe99b0e4d7c25e
Lenard Szolnoki <leni536@gmail.com>
2025-02-16 16:29:20 +0000

    Initial commit

---
+++ generate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+num=2413
+stat="P"
+repo_state=
+
+if [ -n "$(git status --untracked-files=no --porcelain)" ]; then
+    repo_state="(dirty)"
+    stat="D"
+fi
+
+revision=$(git tag --points-at HEAD | grep '^rev' | cut -b4-)
+if [ -z "$revision" ]; then
+    revision="commit_$(git rev-parse --short HEAD)"
+    stat="D"
+fi
+revision="${revision}${repo_state}"
+shortname="${stat}${num}"
+
+echo "generate ${stat}${num}R${revision}"
+
+bikeshed --md-status="${stat}" --md-shortname="${shortname}" --md-revision="${revision}" spec
